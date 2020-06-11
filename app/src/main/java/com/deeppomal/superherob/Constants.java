6d43606850b96c1dba6fa54e84bf6f3f86871e8c
deeppomal <deeppomal98@gmail.com>
2020-06-11 10:29:43 +0530

    initial commit

--- app/src/main/java/com/deeppomal/superherob/Constants.java
+++ app/src/main/java/com/deeppomal/superherob/Constants.java
@@ -7,6 +7,5 @@ public class Constants {
     public static final String FIREBASE_LOCATION_USERS = "users";
     public static final String FIREBASE_URL_USERS = FIREBASE_URL + "/" + FIREBASE_LOCATION_USERS;
     public static final String FIREBASE_PROPERTY_TIMESTAMP = "timestamp";
-    public static final String NEW = "timestamp";
 
 }
